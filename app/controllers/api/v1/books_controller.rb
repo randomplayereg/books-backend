b075aa8629e9a0c38485c737b1e5f9646e0ace07
emil-rnd <randomplayer.eg@gmail.com>
2018-04-17 10:05:34 +0700

    remove debugger

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -75,7 +75,6 @@ module Api
       private
         # Use callbacks to share common setup or constraints between actions.
         def set_book
-          debugger
           @book = Book.find(params[:id])
         end
 
