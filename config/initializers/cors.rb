97c221889f769477aa3c2e40538d98542fecfb66
emil-rnd <randomplayer.eg@gmail.com>
2018-05-02 09:15:20 +0700

    exposing headers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins ENV.fetch('ALLOWED_ORIGINS') { '*' }
 
     resource '*',
-      headers: :any, :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client']
+      headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
